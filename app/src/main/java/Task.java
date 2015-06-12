eb339fe33c6eece14e9fdced81ced62eb44716ae
neoterraforma <himanshu23@icloud.com>
2015-06-12 00:15:26 -0400

    first commit

---
+++ app/src/main/java/Task.java
@@ -0,0 +1,34 @@
+import java.util.Date;
+
+/**
+ * Created by Himanshu on 6/11/2015.
+ */
+public class Task {
+    private String mName;
+    private Date mDueDate;
+    private boolean mDone;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public Date getDueDate() {
+        return mDueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        mDueDate = dueDate;
+    }
+
+    public boolean isDone() {
+        return mDone;
+    }
+
+    public void setDone(boolean done) {
+        mDone = done;
+    }
+}
